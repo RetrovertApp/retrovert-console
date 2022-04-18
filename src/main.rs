1aa1c7e7fbe8d90097b8c8394bcdaa8bf8c6fbbc
Daniel Collin <daniel@collin.com>
2022-04-18 18:20:01 +0200

    More error handling

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,26 @@ use log::{debug, error, info, warn};
 fn main() -> Result<()> {
     Core::init_logging()?;
 
-    error!("Bright red error");
-    info!("This only appears in the log file");
-    debug!("This level is currently not enabled for any logger");
+    if let Err(e) = Core::init_data_directory() {
+        error!("Unable to init directories: error {:?}", e);
+        return Err(e);
+    }
 
-    let loaded_core = Core::load_core()?;
-    let mut core = Core::new(&loaded_core)?;
+    let loaded_core = match Core::load_core() {
+        Ok(lib) => lib,
+        Err(e) => {
+            error!("Unable to load corelib: {}", e);
+            return Err(e);
+        }
+    };
+
+    let core = match Core::new(&loaded_core) {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Unable to create core {}", e);
+            return Err(e);
+        }
+    };
 
     let core_instance = (core.core_create_func)();
     (core.core_update_func)(core_instance);
