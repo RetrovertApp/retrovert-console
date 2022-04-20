163e4ed9c9076ac2264355a12744c36275b5faff
Daniel Collin <daniel@collin.com>
2022-04-20 09:47:02 +0200

    WIP on setup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use core_loader::Core;
-use log::{debug, error, info, warn};
+use log::error;
+
+const HELP_NO_CORE: &str = "\
+retrovert-console
+USAGE:
+  retrovert-console [ARGS]
+  --core        PATH    Override path to core library
+";
+
+const HELP_CORE: &str = "\
+retrovert-console
+USAGE:
+  retrovert-console [ARGS]
+  --core        PATH    Override path to core library
+";
 
 fn main() -> Result<()> {
     Core::init_logging()?;
 
-    if let Err(e) = Core::init_data_directory() {
-        error!("Unable to init directories: error {:?}", e);
-        return Err(e);
-    }
+    let mut pargs = pico_args::Arguments::from_env();
 
-    let loaded_core = match Core::load_core() {
-        Ok(lib) => lib,
+    // Help has a higher priority and should be handled separately.
+    //if pargs.contains(["-h", "--help"]) {
+    //    print!("{}", HELP);
+    //    return Ok(());
+    //}
+
+    // override core location
+    let core_over: Option<String> = pargs.opt_value_from_str("--core")?;
+
+    let loaded_core = match Core::load_core(&core_over) {
+        Result::Ok(lib) => lib,
         Err(e) => {
             error!("Unable to load corelib: {}", e);
             return Err(e);
@@ -19,12 +39,19 @@ fn main() -> Result<()> {
     };
 
     let core = match Core::new(&loaded_core) {
-        Ok(c) => c,
+        Result::Ok(c) => c,
         Err(e) => {
             error!("Unable to create core {}", e);
+            print!("{}", HELP_NO_CORE);
             return Err(e);
         }
     };
+
+    if pargs.contains(["-h", "--help"]) {
+        println!("{}", HELP_CORE);
+        (core.core_show_args);
+        return Ok(());
+    }
 
     let core_instance = (core.core_create_func)();
     (core.core_update_func)(core_instance);
