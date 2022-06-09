e0b61a29be54c00019631ec933c19a9d91cd0099
Daniel Collin <daniel@collin.com>
2022-06-09 15:00:04 +0200

    Support loading from cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{Ok, Result};
 use core_loader::Core;
 use log::error;
+use pancurses::{Input};
+use cfixed_string::CFixedString;
 
 const HELP: &str = "\
 retrovert-console
@@ -12,14 +14,6 @@ fn main() -> Result<()> {
     Core::init_logging()?;
 
     let mut pargs = pico_args::Arguments::from_env();
-
-    dbg!(&pargs);
-
-    // Help has a higher priority and should be handled separately.
-    //if pargs.contains(["-h", "--help"]) {
-    //    print!("{}", HELP);
-    //    return Ok(());
-    //}
 
     // override core location
     let core_over: Option<String> = pargs.opt_value_from_str("--core")?;
@@ -48,7 +42,41 @@ fn main() -> Result<()> {
     }
 
     let core_instance = (core.core_create_func)();
-    (core.core_update_func)(core_instance);
+
+    /*
+    let window = pancurses::initscr();
+    window.printw("Type things, press delete to quit\n");
+    window.keypad(true);
+    pancurses::noecho();
+
+    loop {
+        window.refresh();
+        match window.getch() {
+            Some(Input::Character(c)) => { window.addch(c); },
+            Some(Input::KeyBackspace) => break,
+            Some(input) => { window.addstr(&format!("{:?}", input)); },
+            None => ()
+        }
+    
+        (core.core_update_func)(core_instance);
+    }
+
+    pancurses::endwin();
+     */
+    
+     if let Some(play_url) = pargs.opt_value_from_str::<_, String>("--play")? {
+        let name_c = CFixedString::from_str(&play_url);
+        //let name = b"ftp://ftp.modland.com/pub/modules/Protracker/Virgill/eurobeat.mod\0";
+        //let name = b"/home/emoon/Music/ftp.modland.com/pub/modules/Protracker/Lizardking/elimination.mod\0";
+        (core.core_load_url)(core_instance, name_c.as_ptr() as _);
+     }
+
+
+    loop {
+        (core.core_update_func)(core_instance);
+        std::thread::sleep(std::time::Duration::from_millis(16));
+    }
+
     (core.core_destroy_func)(core_instance, false);
 
     Ok(())
